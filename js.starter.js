1064b72f3000798ac1029b943a8946f8f0875993
Shamim <35076893+sphp@users.noreply.github.com>
2018-11-26 23:13:27 +0600

    Update js.starter.js
    vary basic javascript starter object only for selection and loop over objects

--- js.starter.js
+++ js.starter.js
@@ -1,19 +1,37 @@
-<script>
 (function(){
-	var japp = function(){
-		function japp(a){
-			return a;
+	"use strict";
+	var  w=window, d=w.document;
+	w.byid  = function(a,b){return (b||d)["getElementById"](a)};
+	w.bytag = function(a,b){return (b||d)["getElementsByTagName"](a)};
+	w.bycls = function(a,b){return (b||d)["getElementsByClassName"](a)};
+	var jap = function(){
+		function jap(a, b){
+			if (!a) return;
+			if (a instanceof jap) return a;
+			var elm = a;
+			if (typeof a === 'string'){
+				if(a.indexOf(" ")>-1) elm = d.querySelectorAll(a);
+				else{
+					a =a.match(/^(\W)?(.*)/);
+					elm = (b||d)["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);
+				}
+			}else if (typeof a === 'function'){
+				return this.ready(a);
+			}
+			if (elm.nodeType || elm === w) elm = [elm];
+			this.length = elm.length;
+			for (var i = 0, l = this.length; i < l; i++) this[i] = elm[i];
 		}
-		japp.prototype.init = function(a) {
-			return new japp(a);
+		jap.prototype.init = function(a, b){
+			return new jap(a, b);
 		};
-		return this;
+		return jap;
 	}();
-	app = japp.prototype;
-	app.fn1 = function(){
+	jap.prototype.each = function(cb){
+		for (var i = 0, ln = this.length; i < ln; i++){
+			if (cb.call(this[i], i, this[i]) === false) break;
+		}
 		return this;
 	};
-	window['$'] = app.init;
+	window['$'] = jap.prototype.init;
 })();
-document.querySelector('body').innerHTML = $('div');
-</script>
